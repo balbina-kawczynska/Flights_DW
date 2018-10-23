848f09afe19f053b146861e032c06ff5432e6ccb
Balbina Kawczyńska <balbina.kawczynska@microsoft.wsei.edu.pl>
2018-10-24 01:01:55 +0200

    Add procedures for inserting Unknown value in Dim tables
    Former-commit-id: 307a3a88ac62f7b4d313949031506a1383a68cee

---
+++ SQL_SD/dbo/Stored Procedures/DimAirport_InsertUnknown.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROC [DimAirport_InsertUnknown]
+AS
+BEGIN
+
+SET IDENTITY_INSERT [DimAirport] ON
+
+INSERT INTO [DimAirport] ([AirportID],[Iata],[AirportName],[City],[State],[Country],[Latitude],[Longtitude])   
+VALUES (-1,'Unknown','Unknown','Unknown','Unknown','Unknown',-1,-1);
+
+SET IDENTITY_INSERT [DimAirport] OFF
+
+END;
