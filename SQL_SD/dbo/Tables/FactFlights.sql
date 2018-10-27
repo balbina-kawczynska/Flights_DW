4e26adb3ed18dbd3b18cccd869f9f3e37974c10f
Balbina Kawczyńska <balbina.kawczynska@microsoft.wsei.edu.pl>
2018-10-27 19:45:53 +0200

    patchess
    Former-commit-id: fb0d10eae59b616b3e76f02b079558126d23e005

--- SQL_SD/dbo/Tables/FactFlights.sql
+++ SQL_SD/dbo/Tables/FactFlights.sql
@@ -4,32 +4,36 @@
     [Month]             TINYINT       NULL,
     [DayofMonth]        TINYINT       NULL,
     [DayOfWeek]         TINYINT       NULL,
-    [DepTime]           SMALLINT      NULL,
+    [DepTime]           NVARCHAR (4)  NULL,
     [CRSDepTime]        SMALLINT      NULL,
-    [ArrTime]           SMALLINT      NULL,
+    [ArrTime]           NVARCHAR (4)  NULL,
     [CRSArrTime]        SMALLINT      NULL,
     [UniqueCarrier]     NVARCHAR (5)  NULL,
     [FlightNum]         INT           NULL,
     [TailNum]           NVARCHAR (10) NULL,
-    [ActualElapsedTime] SMALLINT      NULL,
+    [ActualElapsedTime] NVARCHAR (4)  NULL,
     [CRSElapsedTime]    SMALLINT      NULL,
-    [AirTime]           SMALLINT      NULL,
-    [ArrDelay]          SMALLINT      NULL,
-    [DepDelay]          SMALLINT      NULL,
+    [AirTime]           NVARCHAR (4)  NULL,
+    [ArrDelay]          NVARCHAR (4)  NULL,
+    [DepDelay]          NVARCHAR (4)  NULL,
     [Origin]            NVARCHAR (5)  NULL,
     [Dest]              NVARCHAR (7)  NULL,
-    [Distance]          SMALLINT      NULL,
-    [TaxiIn]            TINYINT       NULL,
-    [TaxiOut]           TINYINT       NULL,
+    [Distance]          NVARCHAR (4)  NULL,
+    [TaxiIn]            NVARCHAR (4)  NULL,
+    [TaxiOut]           NVARCHAR (4)  NULL,
     [Cancelled]         BIT           NULL,
     [CancellationCode]  NVARCHAR (10) NULL,
     [Diverted]          BIT           NULL,
-    [CarrierDelay]      SMALLINT      NULL,
-    [WeatherDelay]      SMALLINT      NULL,
-    [NASDelay]          SMALLINT      NULL,
-    [SecurityDelay]     SMALLINT      NULL,
-    [LateAircraftDelay] SMALLINT      NULL
+    [CarrierDelay]      NVARCHAR (4)  NULL,
+    [WeatherDelay]      NVARCHAR (4)  NULL,
+    [NASDelay]          NVARCHAR (4)  NULL,
+    [SecurityDelay]     NVARCHAR (4)  NULL,
+    [LateAircraftDelay] NVARCHAR (4)  NULL
 );
+
+
+
+
 
 
 
